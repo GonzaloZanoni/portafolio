0097a016bd4b0ccf807d4defd6f672cfd5fe71c9
GonzaloZanoni <Gonzalozanoni3@gmail.com>
2023-10-30 01:15:34 -0300

    inicio de la web porfolio

---
+++ src/redes_trabajos.js
@@ -0,0 +1,105 @@
+const REDES_SOCIALES = {
+  github: 'https://github.com/jaenfigueroa',
+  facebook: 'https://www.facebook.com/JaenDeveloper/',
+  twitter: 'https://twitter.com/JaenFigueroa_',
+  linkedin: 'https://www.linkedin.com/in/jaenfigueroa/',
+  correo: 'contact@jaenfigueroa.com'
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+const TRABAJOS = [
+  // {
+  //   nombre: 'Puzzle Shuerk',
+  //   fecha: '28 - Oct - 2022',
+  //   descripcion:
+  //     'Game where you have to drag and drop to finish assembling the puzzle.',
+  //   website: 'https://jaenfigueroa.github.io/Rompecabezas/',
+  //   github: 'https://github.com/jaenfigueroa/Rompecabezas',
+  //   imagen: './assets/proyectos/puzzle_shuerk.png'
+  // },
+  {
+    nombre: 'Figuras Geométricas',
+    fecha: '30 - Abr - 2023',
+    descripcion: 'Juego drag and drop con figuras geométricas',
+    website: 'https://gonzalozanoni.github.io/FigurasGeom%C3%A9tricas/',
+    github: 'https://github.com/GonzaloZanoni/GonzaloZanoni.github.io.git',
+    imagen: './assets/proyectos/figurasGeometricas.jpg'
+  }
+  // {
+  //   nombre: 'Halloween Pumpkin',
+  //   fecha: '1 - Nov - 2022',
+  //   descripcion:
+  //     'A personal project for halloween, an animation using only html and css.',
+  //   website: 'https://jaenfigueroa.github.io/Halloween-Pumpkin/',
+  //   github: 'https://github.com/jaenfigueroa/Halloween-Pumpkin',
+  //   imagen: './assets/proyectos/halloween_pumpkin.png'
+  // },
+  // {
+  //   nombre: 'Cubo Rubik',
+  //   fecha: '18 - Nov - 2022',
+  //   descripcion:
+  //     "A personal project, an animation of a rubik's cube in 3d using only html and css.",
+  //   website: 'https://jaenfigueroa.github.io/3D-Rubiks-Cube/',
+  //   github: 'https://github.com/jaenfigueroa/3D-Rubiks-Cube',
+  //   imagen: './assets/proyectos/cubo_rubik.png'
+  // },
+  // {
+  //   nombre: 'JaenCraft',
+  //   fecha: '16 - Nov - 2022',
+  //   descripcion:
+  //     'Minecraft game made only with CSS and Javascript, where you can move the character with the keys, change the person, etc.',
+  //   website: 'https://jaenfigueroa.github.io/JaenCraft/',
+  //   github: 'https://github.com/jaenfigueroa/JaenCraft',
+  //   imagen: './assets/proyectos/jaencraft.png'
+  // },
+  // {
+  //   nombre: 'Pomodoro Timer',
+  //   fecha: '03 - Dec - 2022',
+  //   descripcion:
+  //     'It is used to take the remaining time, where you can set the minutes and seconds in which an alarm should be displayed.',
+  //   website: 'https://jaenfigueroa.github.io/Pomodoro-timer',
+  //   github: 'https://github.com/jaenfigueroa/Pomodoro-timer',
+  //   imagen: './assets/proyectos/pomodoro_timer.png'
+  // },
+  // {
+  //   nombre: 'eCommerce',
+  //   fecha: '10 - Dec - 2022',
+  //   descripcion:
+  //     'Website where different food dishes of different prices and flavors are sold.',
+  //   website: 'https://jaenfigueroa.github.io/eCommerce',
+  //   github: 'https://github.com/jaenfigueroa/eCommerce',
+  //   imagen: './assets/proyectos/ecommerce.png'
+  // },
+  // {
+  //   nombre: 'Web Portfolio',
+  //   fecha: '15 - Dec - 2022',
+  //   descripcion:
+  //     'Website where different food dishes of different prices and flavors are sold.',
+  //   website: 'https://jaenfigueroa.github.io/web-portfolio',
+  //   github: 'https://github.com/jaenfigueroa/web-portfolio',
+  //   imagen: './assets/proyectos/web_portfolio.png'
+  // },
+  // {
+  //   nombre: 'Manage Movies List app',
+  //   fecha: '15 - Dec - 2022',
+  //   descripcion:
+  //     'Application to save movie lists, you can edit them, move them, delete them, search them, etc.',
+  //   website: 'https://manage-movie-list.netlify.app/',
+  //   github: 'https://github.com/jaenfigueroa/manage-movie-list',
+  //   imagen: './assets/proyectos/movies.png'
+  // },
+  // {
+  //   nombre: 'PokeZone',
+  //   fecha: '3 - Jan - 2023',
+  //   descripcion:
+  //     'This project is a Pokémon-themed website created with React.js and the PokeAPI API.',
+  //   website: 'https://pokezone.jaenfigueroa.com/',
+  //   github: 'https://github.com/jaenfigueroa/PokeZone',
+  //   imagen: './assets/proyectos/pokezone.png'
+  // }
+]
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
